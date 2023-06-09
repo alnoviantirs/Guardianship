8b925795a01993fb6d4aa1367e371e27b070efcc
Novi <alnoviantiramadhani91@gmail.com>
2023-06-09 15:17:14 +0700

    ehe

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -1,4 +1,4 @@
-export let urlPOST = "https://alnovianti.herokuapp.com/insert/";
+export let urlPOST = "https://alnovianti.herokuapp.com/insert";
 
 export function AmbilResponse(result) {
   console.log(result); //menampilkan response API pada console
