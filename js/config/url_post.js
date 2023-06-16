26b8d16087fcca01ddc0205965e51cea8ce1689b
Novi <alnoviantiramadhani91@gmail.com>
2023-06-16 14:57:11 +0700

    UAS

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -3,5 +3,5 @@ export let urlPOST = "https://alnovianti.herokuapp.com/insperwalian";
 export function AmbilResponse(result) {
   console.log(result); //menampilkan response API pada console
   alert(result.message); //menampilkan response API pada alert
-  window.location.href("index.html"); //reload halaman setelah klik ok pada alert
+  window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
 }
