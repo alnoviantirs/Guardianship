fabb46a97668253a1209cb1873aeb45f783bfa4e
Fedhira Syaila <pedirasyaila@gmail.com>
2023-06-20 18:24:09 +0700

    bgg

--- js/config/url_put.js
+++ js/config/url_put.js
@@ -1,15 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const perwalianId = urlParams.get("perwalianId");
-const dosenId = urlParams.get("dosenId");
-const mahasiswaId = urlParams.get("mahasiswaId");
-const ruanganId = urlParams.get("ruanganId");
 
 export let urlPUT = "https://alnovianti.herokuapp.com/update/" + perwalianId;
-export let urlPUTDosen = "https://alnovianti.herokuapp.com/upddosen/" + dosenId;
-export let urlPUTMahasiswa =
-  "https://alnovianti.herokuapp.com/updmahasiswa/" + mahasiswaId;
-export let urlPUTRuangan =
-  "https://alnovianti.herokuapp.com/updruangan/" + ruanganId;
 
 export function AmbilResponse(result) {
   console.log(result); //menampilkan respond API pada console
