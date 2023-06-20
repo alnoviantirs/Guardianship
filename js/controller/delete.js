aaac76e0a64a7ba5465ce1673a19d613d9bea29d
Novi <alnoviantiramadhani91@gmail.com>
2023-06-20 15:26:52 +0700

    VHJG

--- js/controller/delete.js
+++ js/controller/delete.js
@@ -1,13 +1,6 @@
 function deleteData(IDHAPUS) {
   var perwalianId = IDHAPUS;
-  var dosenId = IDHAPUS;
-  var mahasiswaId = IDHAPUS;
-  var ruanganId = IDHAPUS;
   var target_url = "https://alnovianti.herokuapp.com/del/" + perwalianId;
-  var target_url = "https://alnovianti.herokuapp.com/deldosen/" + dosenId;
-  var target_url =
-    "https://alnovianti.herokuapp.com/delmahasiswa/" + mahasiswaId;
-  var target_url = "https://alnovianti.herokuapp.com/delruangan/" + ruanganId;
 
   var requestOptions = {
     method: "DELETE",
